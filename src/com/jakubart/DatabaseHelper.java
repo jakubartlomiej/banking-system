2743d84e768cfe8bb6ff9933d0a64f3c7bd80eff
bjakubowski <bartakj141@wp.pl>
2020-11-09 16:17:09 +0100

    add method connection to database

--- src/com/jakubart/DatabaseHelper.java
+++ src/com/jakubart/DatabaseHelper.java
@@ -1,10 +1,29 @@
 package com.jakubart;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DatabaseHelper {
 
     private static final String url = "jdbc:sqlite:C:\\Users\\b.jakubowski\\Documents\\sqllite\\banking-system\\banking.db";
     private static Connection conn;
-    
+
+    public static void getConnection() {
+
+        try {
+            conn = DriverManager.getConnection(url);
+            System.out.println("Connection to SQLite has been established.");
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        } finally {
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        }
+    }
 }
