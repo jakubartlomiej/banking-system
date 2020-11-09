f78bb8bec6b82aa8de19b79f0f844149f703f096
bjakubowski <bartakj141@wp.pl>
2020-11-09 16:01:41 +0100

    add class DatabaseHelper

---
+++ src/com/jakubart/DatabaseHelper.java
@@ -0,0 +1,10 @@
+package com.jakubart;
+
+import java.sql.Connection;
+
+public class DatabaseHelper {
+
+    private static final String url = "jdbc:sqlite:C:\\Users\\b.jakubowski\\Documents\\sqllite\\banking-system\\banking.db";
+    private static Connection conn;
+    
+}
