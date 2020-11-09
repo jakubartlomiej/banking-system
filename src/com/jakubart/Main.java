ec64427b1d1533952f957cc10e772c3ef2c94bae
bjakubowski <bartakj141@wp.pl>
2020-11-09 16:17:41 +0100

    add method connect to db to main class

--- src/com/jakubart/Main.java
+++ src/com/jakubart/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-
+        DatabaseHelper.getConnection();
         Scanner scanner = new Scanner(System.in);
         int choice;
         String cardNoumber;
