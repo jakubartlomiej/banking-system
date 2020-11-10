169f008604d53c0c74c5c9859fd1e930f870af5f
bjakubowski <bartakj141@wp.pl>
2020-11-10 21:16:22 +0100

    added class Menu to main and added new switch options

--- src/com/jakubart/Main.java
+++ src/com/jakubart/Main.java
@@ -9,9 +9,8 @@ public class Main {
         int choice;
         Bank bank = new Bank("banking.db");
         Account account = new Account();
-        System.out.println("1. Create an account");
-        System.out.println("2. Log into account");
-        System.out.println("0. Exit");
+        Menu menu = new Menu();
+        menu.main();
         while (true) {
             System.out.print(">");
             choice = scanner.nextInt();
@@ -31,53 +30,76 @@ public class Main {
                     }
                     account.setCardNoumber("");
                     account.setPin("");
-                    System.out.println("1. Create an account");
-                    System.out.println("2. Log into account");
-                    System.out.println("0. Exit");
+                    menu.main();
                     break;
                 case 2:
                     System.out.print("Enter your card number:\n>");
                     account.setCardNoumber(scanner.next());
                     System.out.print("Enter your PIN:\n>");
                     account.setPin(scanner.next());
-                    if (bank.login(account.getCardNoumber(),account.getPin())) {
+                    if (bank.login(account.getCardNoumber(), account.getPin())) {
                         System.out.println("You have successfully logged in!\n");
-                        System.out.println("1. Balance");
-                        System.out.println("2. Log out");
-                        System.out.println("0. Exit");
+                        menu.account();
                         do {
                             System.out.print(">");
                             choice = scanner.nextInt();
                             switch (choice) {
                                 case 1:
                                     System.out.println("Balance: " + bank.checkAccountBalance(account.getCardNoumber()) + "\n");
-                                    System.out.println("1. Balance");
-                                    System.out.println("2. Log out");
-                                    System.out.println("0. Exit");
+                                    menu.account();
                                     break;
                                 case 2:
+                                    System.out.print("Enter income: \n>");
+                                    if (bank.addIncome(scanner.nextInt(), account.getCardNoumber())) {
+                                        System.out.println("Income was added!");
+                                    }
+                                    menu.account();
+                                    break;
+                                case 3:
+                                    System.out.println("Transfer");
+                                    System.out.print("Enter card number:\n>");
+                                    String transferCard = scanner.next();
+                                    if (transferCard.equals(account.getCardNoumber())) {
+                                        System.out.println("You can't transfer money to the same account!\n");
+                                        menu.account();
+                                    } else if (account.luhnaAlgorithmGenerator(transferCard.toCharArray()) != Integer.parseInt(String.valueOf(transferCard.charAt(transferCard.length() - 1)))) {
+                                        System.out.println("Probably you made mistake in the card number. Please try again!\n");
+                                        menu.account();
+                                    } else if (!bank.checkIfExistAccount(transferCard)) {
+                                        System.out.println("Such a card does not exist.\n");
+                                        menu.account();
+                                    } else {
+                                        System.out.print("Enter how much money you want to transfer::\n>");
+                                        int amount = scanner.nextInt();
+                                        if (amount > bank.checkAccountBalance(account.getCardNoumber())) {
+                                            System.out.println("Not enough money!\n");
+                                        } else {
+                                            if (bank.doTransfer(amount, transferCard, account.getCardNoumber())) {
+                                                System.out.println("Success!\n");
+                                            }
+                                        }
+                                        menu.account();
+                                    }
+                                    break;
+                                case 5:
                                     account.setId(0);
                                     account.setCardNoumber("");
                                     account.setPin("");
                                     account.setBalance(0);
                                     System.out.println("You have successfully logged out!\n");
-                                    System.out.println("1. Create an account");
-                                    System.out.println("2. Log into account");
-                                    System.out.println("0. Exit");
+                                    menu.main();
                                     break;
                                 case 0:
                                     System.out.println("Bye !");
                                     System.exit(1);
                             }
-                        } while (choice != 2);
+                        } while (choice != 5);
 
                     } else {
                         account.setCardNoumber("");
                         account.setPin("");
                         System.out.println("Wrong card number or PIN!\n");
-                        System.out.println("1. Create an account");
-                        System.out.println("2. Log into account");
-                        System.out.println("0. Exit");
+                        menu.main();
                     }
                     break;
                 case 0:
